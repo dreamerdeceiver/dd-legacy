4708d86bca9dcbfa5acf6d5395cfee14d195bd79
dreamerdeceiver <90097940+dreamerdeceiver@users.noreply.github.com>
2021-12-18 09:42:54 +0300

    без цикла

--- binary_search.py
+++ binary_search.py
@@ -1,7 +1,7 @@
 def inptr(lst):
     lst = list()
     inp = input('Введите что-нибудь: ')
-    while inp != '': 
+    while inp != '':
         lst.append(int(inp))
         inp = input('Введите что-нибудь: ')
     return lst
@@ -12,11 +12,9 @@ def bnr_srch(list, key):
     ppr_bnd = len(list) - 1
     while lwr_bnd <= ppr_bnd:
         md = (lwr_bnd + ppr_bnd) // 2
-        if list[md] == key:   
-            for n in range(md + 1):
-                if list[md] == list[n] and n < md:
-                    md = n
-            return md
+        if list[md] == key:
+            frst_occ = list.index(key)
+            return frst_occ
         else:
             if key < list[md]:
                 ppr_bnd = md - 1
